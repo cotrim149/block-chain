d83edb7af824cc504b1a46c47ab9a3c456dce778
Victor Cotrim <cotrim149@gmail.com>
2016-11-15 16:30:03 -0200

    Adding initial script for create voter wallet

--- geth_utils/create_candidate_wallet.sh
+++ geth_utils/create_candidate_wallet.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
+
 geth --password std_passw account new > geth_address_output
 cat geth_address_output| cut -d' ' -f2 | cut -d'{' -f2 | cut -d'}' -f1 > wallet
 rm geth_address_output
-cat wallet
---
+++ geth_utils/create_voter_wallet.sh
@@ -0,0 +1,1 @@
+sh create_candidate_wallet.sh
