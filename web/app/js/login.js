624828352a66a84e08acc62ba540c6234c5db7a9
deniprogram <denissonfernades@hotmail.com>
2016-11-15 23:54:35 -0200

    web3

--- web/app/js/login.js
+++ web/app/js/login.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	
+	var Web3 = require('web3');
+    var web3 = new Web3();
+    var eth = web3.eth; //Contém os métodos relacionados ethereum blockchain.
+
+    web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+    var version = web3.version.api;
+	console.log(version); // "0.2.0"
 });
