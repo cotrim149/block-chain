04c70950c369d21d3a2994e733c954ee29c34535
Victor Cotrim <cotrim149@gmail.com>
2016-11-16 23:18:40 -0200

    Adding script to create voters.json automatically

---
+++ geth_utils/seed/create_voter_json.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+from json import JSONEncoder
+from subprocess import call
+
+myList = []
+myList.append("[")
+limit = 4
+
+for x in range(1,limit):
+    call(["sh", "create_wallet.sh"])
+    walletFile = open('wallet','r')
+
+    jsonString = JSONEncoder().encode({
+      "id":x,
+      "name": "Joao da Silva " + str(x),
+      "estado": "GO",
+      "titulo": "123412341234",
+      "zone": "12"+str(x),
+      "section":"4"+str(x)+"6",
+      "wallet": str.strip(walletFile.read())
+    })
+    myList.append(jsonString)
+    if x != limit-1:
+        myList.append(",")
+    print jsonString
+
+myList.append("]")
+
+f = open('voters.json','w')
+for x in myList:
+    f.write(x)
+f.close()
