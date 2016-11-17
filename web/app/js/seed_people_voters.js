a5ce61e6a994e2695d4fed68d0e9c0af5f42cfc5
deniprogram <denissonfernades@gmail.com>
2016-11-17 15:11:35 -0200

    Refatorando fluxo de voto

--- web/app/js/seed_people_voters.js
+++ web/app/js/seed_people_voters.js
@@ -1,13 +1,25 @@
 var people_voters = [{
-	id: 1,
-	name: 'Joao da Silva',
-	estado: 'GO',
-	titulo_eleitoral:"123232",
-	wallet: '0x6a5b342ec71def8aac337b82969d9ddd811023c9'
-},{
-	id: 2,
-	name: 'Maria da Costa',
-	estado: 'PI',
-	titulo_eleitoral:"44353",
-	wallet: '0x6a5b342ec71def8aac337b82969d9ddd811023c9'
+	"name": "Joao da Silva 1",
+	"zone": "121",
+	"section": "416",
+	"wallet": "1f8d04a84506f0fce4d9b699b11e9a2a258be9ff",
+	"titulo_eleitoral": "123412341234",
+	"estado": "DF",
+	"id": 1
+}, {
+	"name": "Joao da Silva 2",
+	"zone": "122",
+	"section": "426",
+	"wallet": "1f8d04a84506f0fce4d9b699b11e9a2a258be9ff",
+	"titulo_eleitoral": "123412341233",
+	"estado": "DF",
+	"id": 2
+}, {
+	"name": "Joao da Silva 3",
+	"zone": "123",
+	"section": "436",
+	"wallet": "1f8d04a84506f0fce4d9b699b11e9a2a258be9ff",
+	"titulo_eleitoral": "123412341235",
+	"estado": "DF",
+	"id": 3
 }];
--- web/app/js/vote.js
+++ web/app/js/vote.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+	var Web3 = require('web3');
+    var web3 = new Web3();
+    var eth  = web3.eth; //Contém os métodos relacionados ethereum blockchain.
+   
+    // Conexão com o BlockChain
+    web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+    var abiVotaCoin = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"marketAddress","type":"address"}],"name":"updatePokeMarketAddress","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"type":"function"},{"constant":true,"inputs":[],"name":"standard","outputs":[{"name":"","type":"string"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"pokeMarketAddress","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"qtdCoinsToDelete","type":"uint256"}],"name":"vanishCoins","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"newSupply","type":"uint256"}],"name":"issueNew","outputs":[],"type":"function"},{"constant":false,"inputs":[],"name":"owned","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"account1Demo","type":"address"},{"name":"account2Demo","type":"address"}],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}];
+    var votaCoinAddress = ' 0xf37e1528dab150ec911199b2a935fbbb4d962e47';
+   	
+   	// Carregando contrato
+    var votaCoinContract = web3.eth.contract(abiVotaCoin);
+	var votaCoin = votaCoinContract.at(votaCoinAddress);
+
 	var user_logged 	 = JSON.parse(localStorage.getItem("login"));
 	var order_candidates = JSON.parse(localStorage.getItem("order_candidates"));
 
@@ -14,25 +28,27 @@ $(document).ready(function() {
 	var broken_candidate 		= $(".broken_candidate");
 	var img_candidate    		= $(".img_candidate");
 	var confirmation_vote 		= $("#confirmation_vote");
+	var vote_null 				= $("#vote_null");
+	var cancel_vote 			= $("#cancel_vote");
 	var end_vote 				= $("#end_vote");
+	var find_candidate			= {};
 	
-	container_details.hide();
 	type_candidate.html(order_candidates[0].label);   
 	
 	brn_vote.click(function(){
 		find_candidate = _.find(seed_candidates, {'tipo':order_candidates[0].id,'numero':$("#numberCandidate").val(),'estado':user_logged.estado});
 		
 		if(find_candidate){
-		 	container_vote.fadeOut();
-		 	container_details.fadeIn();
+		 	container_vote.hide();
+		 	container_details.removeClass("hide");
+		 	container_details.addClass("show");
 		 	loadCandidate(find_candidate);
-		 	nextCandidate();
-		 	confirmVote(find_candidate);
 		}else{
 			alertify.error(order_candidates[0].label+' não encontrado.');
 		}
 	});
 
+	//** Carrega os dados do candidato **//
 	function loadCandidate(obj){
 		number_show_candidate.html(obj.numero);
 		name_candidate.html(obj.name);
@@ -42,21 +58,56 @@ $(document).ready(function() {
 		img_candidate.attr("src",obj.photo);
 	}
 
-	function confirmVote(candidate){
-		confirmation_vote.click(function(){
-			if(candidate.tipo != 5){
-				window.location = "vote.html";
-			}else{
-				container_details.hide();
-				end_vote.removeClass("hide");
-				setTimeout(function(){window.location = "index.html";}, 2000);
-			}
-		});
-	}
-
+	//** Proximo candidato a ser votado **//
 	function nextCandidate(){
 		_.remove(order_candidates, function(n) {return n == order_candidates[0];});
 		localStorage.setItem("order_candidates", JSON.stringify(order_candidates));
 	}
 
+	//** Confirmar voto **///
+	function confirmVote(candidate){
+		confirmation_vote.click(function(){
+			nextCandidate();
+			if(find_candidate.tipo != 5){
+				window.location = "vote.html";
+			}else{
+				container_details.removeClass("show");
+		 		container_details.addClass("hide");
+				end_vote.removeClass("hide");
+				document.getElementById("audio_play").play();
+				setTimeout(function(){window.location = "index.html";}, 2000);
+			}
+		});
+	}
+	confirmVote();
+
+	//** Cancelar voto **//
+	function cancelVote(){
+		cancel_vote.click(function(){
+			container_vote.show();
+			//container_details.hide();
+			container_details.removeClass("show");
+		 	container_details.addClass("hide");
+		});
+	}
+	cancelVote()
+
+	//** Votar em branco **//
+	function voteNull(){
+		candidate = order_candidates[0];
+		vote_null.click(function(){
+			if(candidate.id != 5){
+				nextCandidate();
+				window.location = "vote.html";
+			}else{
+				container_vote.hide();
+				container_details.removeClass("show");
+		 		container_details.addClass("hide");
+				end_vote.removeClass("hide");
+				document.getElementById("audio_play").play();
+				setTimeout(function(){window.location = "index.html";}, 2000);
+			}
+		});
+	}
+	voteNull();
 });
