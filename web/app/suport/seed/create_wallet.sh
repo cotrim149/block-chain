a20932ee3dc6f4f09fb424c65eb6bd2e47a775e9
Victor Cotrim <cotrim149@gmail.com>
2016-11-17 19:51:12 -0200

    .gitignore is now working

--- web/app/suport/seed/create_wallet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-geth --password std_passw --keystore private_net/keystore account new > geth_address_output
-cat geth_address_output| cut -d' ' -f2 | cut -d'{' -f2 | cut -d'}' -f1 > wallet
-rm geth_address_output
-cat wallet
