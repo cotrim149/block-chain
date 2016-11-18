026f8e8af221530c9e4080c5cfa5ea49cb4e1ba8
deniprogram <denissonfernades@gmail.com>
2016-11-18 14:29:17 -0200

    Ajustes

--- web/app/js/dashboard.js
+++ web/app/js/dashboard.js
@@ -3,16 +3,29 @@ $(document).ready(function () {
 
     _.forEach(order_candidates, function(value, key) {
         list_type_candidate.append(insertHtmlTypeCandidate(value));
-      console.log(value,key);
     });
 
     function insertHtmlTypeCandidate(value){
       html = '<div class="list-group col-md-2" data-type_candidate="'+value.id+'">'
-        html += '<a href="#" class="list-group-item active">'
+        html += '<a class="list-group-item active">'
         html += value.label
         html += '</a>'
       html += '</div>'  
       return html;
     }
+
+    function insertHtmlCandidate(value){
+      html = '<a href="#" class="list-group-item">'
+        html += value.name
+      html += '</a>'
+      return html;
+    }
+
+    _.forEach(seed_candidates, function(value, key) {
+      container_type_candidate = $("[data-type_candidate='"+value.tipo+"']");
+      if(container_type_candidate){
+        container_type_candidate.append(insertHtmlCandidate(value));
+      }
+    });    
 
 });
